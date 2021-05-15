276567225ef7a96a21f2510910a04b51512759ea
Oscar Ballesta <oscarballestach@gmail.com>
2021-05-15 12:48:26 -0500

    Conexión a la base de datos

--- dataBase.php
+++ dataBase.php
@@ -1,5 +1,9 @@
 <?php
-$mysqli = new mysqli("localhost", "root", "", "alevines");
+$host="remotemysql.com:3306";
+$user='tErmskPHFi';
+$contra="XVQL0kPGEc";
+$base="tErmskPHFi";
+$mysqli = new mysqli($host,$user,$contra,$base);
 if ($mysqli->connect_errno) {
     echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
